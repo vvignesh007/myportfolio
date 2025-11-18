67d0eccdd610c7644c5f0eea8c6601848bc71422
rohitsnp <rohitsn@ikm.gov.in>
2025-11-18 12:06:41 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/portfolio/", 
+  base: "/myportfolio/", 
   plugins: [react(),
     tailwindcss()
   ],
